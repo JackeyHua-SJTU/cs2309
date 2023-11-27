7436ce413c9d63be6b6344264f6947d1a2626acb
Jackey Hua <dyhzd2013@163.com>
2023-11-27 10:27:54 +0800

    feat: modify star structure

--- hw7/fltk_w13/star.cpp
+++ hw7/fltk_w13/star.cpp
@@ -21,26 +21,31 @@ void star::draw() {
     
     int w = this->w();
     int h = this->h();
-    int radis = (w > h) ? h / 3 : w / 3;
+    double radius_large = (w > h) ? h / 3 : w / 3;
+    double unit_angle = 2 * M_PI / this->number;
+    double radius_small = radius_large / (2 * cos(unit_angle / 2));
 
-    std::vector<std::pair<int, int>> points;
+    std::vector<std::pair<int, int>> points1;
+    std::vector<std::pair<int, int>> points2;
     std::vector<Fl_Color> colors{FL_RED, FL_GREEN, FL_DARK_BLUE, FL_DARK_MAGENTA, FL_DARK_CYAN};
-    for (int i = 0; i < this->number - 1; i++) {
-        double angle = 2 * M_PI * i / (this->number - 1);
-        int x = w / 2 + radis * cos(angle);
-        int y = h / 2 - radis * sin(angle);
-        points.emplace_back(x, y);
+    for (int i = 0; i < this->number; i++) {
+        int x1 = w / 2 + radius_large * cos(unit_angle * i);
+        int y1 = h / 2 - radius_large * sin(unit_angle * i);
+        int x2 = w / 2 + radius_small * cos(unit_angle * i + unit_angle / 2);
+        int y2 = h / 2 - radius_small * sin(unit_angle * i + unit_angle / 2);
+        points1.emplace_back(x1, y1);
+        points2.emplace_back(x2, y2);
     }
 
-    int s = points.size();
+    int s = points1.size();
     int color = 0;
     int thickness = 3;
     for (int i = 0 ; i < s; ++i) {
         fl_color(colors[color]);
         color = (color + 1) % colors.size();
         for (int j = 0; j < thickness; ++j) {
-            fl_line(w / 2, h / 2 + j, points[i].first, points[i].second + j);
-            fl_line(points[i].first, points[i].second + j, points[(i + 1) % s].first, points[(i + 1) % s].second + j);
+            fl_line(points1[i].first, points1[i].second, points2[i].first, points2[i].second);
+            fl_line(points2[i].first, points2[i].second, points1[(i + 1) % s].first, points1[(i + 1) % s].second);
         }       
     }
     color = 2;
@@ -49,8 +54,9 @@ void star::draw() {
         color = (color + 1) % colors.size();
         fl_begin_polygon();
         fl_vertex(w / 2, h / 2);
-        fl_vertex(points[i].first, points[i].second);
-        fl_vertex(points[(i + 1) % s].first, points[(i + 1) % s].second);
+        fl_vertex(points1[i].first, points1[i].second);
+        fl_vertex(points2[i].first, points2[i].second);
+        fl_vertex(points1[(i + 1) % s].first, points1[(i + 1) % s].second);
         fl_end_polygon();
     }
 
